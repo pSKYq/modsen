6c73e51bfa376e1e2d323600093c071f82713fb8
pSKYq <92396566+pSKYq@users.noreply.github.com>
2024-06-19 06:13:43 +0300

    Add files via upload

---
+++ data_types/data_types.js
@@ -0,0 +1,7 @@
+function countd(number) {
+    let numberStr = Math.abs(number).toString();
+    return numberStr.length;
+}
+
+let num = 312342;
+console.log(countd(num)); 
---
+++ variables/variables.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var varNumber = 10;
+    let letNumber = 20;
+    const constNumber = 30;
+  
+    console.log('Inside function:');
+    console.log(varNumber);  
+    console.log(letNumber);  
+    console.log(constNumber);  
+  }
+  
+  myFunction();
+  
+  console.log('Outside function:');
+  console.log(varNumber);  
+  console.log(letNumber);  
+  console.log(constNumber);  
+  
